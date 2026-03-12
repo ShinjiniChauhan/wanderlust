64a2d440b296bcfb9c4a9bd21799d95cf81d3899
ShinjiniChauhan <mamtachauhan7560@gmail.com>
2026-03-12 17:13:06 +0530

    Implement image upload and update feature for listing edit form

--- controllers/listings.js
+++ controllers/listings.js
@@ -43,9 +43,17 @@ module.exports.renderEditFrom = async (req,res) => {
 }
 
 module.exports.updateListing = async (req ,res) => {
+
     let {id} =req.params;
-    await Listing.findByIdAndUpdate(id , {...req.body.listing});
-    // console.log(req.body);
+    let listing = await Listing.findByIdAndUpdate(id , {...req.body.listing});
+
+    if(typeof req.file != "undefined"){
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.image = {url, filename};
+    await listing.save();
+    }
+
     req.flash("success" , "Listing Updated!");
     res.redirect(`/listings/${id}`);
 }
