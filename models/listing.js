c3afe8f577ace50baf0c47bc34412961be942fcc
ShinjiniChauhan <mamtachauhan7560@gmail.com>
2026-03-12 16:43:19 +0530

    Implement listing image upload with multer and store image URL in database

--- models/listing.js
+++ models/listing.js
@@ -11,18 +11,8 @@ const listingSchema = new Schema({
         type: String
     },
     image: {
-        filename: {
-            type: String,
-            default: "listingimage"
-        },
-        url: {
-            type: String,
-            default:
-                "https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            set: (v) => v === ""
-                ? "https://images.unsplash.com/photo-1506953823976-52e1fdc0149a?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                : v,
-        }
+        url: String,
+        filename: String,
     },
     price: {
         type: Number,
--- routes/listing.js
+++ routes/listing.js
@@ -5,11 +5,14 @@ const Listing = require("../models/listing");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage }); 
 
 router
 .route("/")
 .get(wrapAsync(listingController.index))
-.post(isLoggedIn, validateListing, wrapAsync (listingController.createListing));
+.post(isLoggedIn,upload.single("listing[image]"),validateListing, wrapAsync (listingController.createListing));
 
 //New Route
 router.get("/new", isLoggedIn , listingController.renderNewFrom);
